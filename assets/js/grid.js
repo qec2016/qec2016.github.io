4659ace90e9303a28ab8da5d5977e44630b3d237
Colson Donohue <colsondonohue@gmail.com>
2016-12-24 01:21:08 -0400

    Refine community grid expansion transition

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -15,20 +15,32 @@
     if (!text.classList.contains('community-text--active')) {
       text.classList.add('community-text--active');
       this.classList.add('community-grid--active');
+      text.style.position = 'static';
       if (activeText) {
         activeText.classList.remove('community-text--active');
         activeGrid.classList.remove('community-grid--active');
+        // wait until transition is complete to change positioning
+        setTimeout(function() {
+          activeText.style.position = 'fixed';
+        }, 300);
       }
-      activeText = text;
-      activeGrid = this;
-      console.log('open');
+      // maintain context in setTimeout
+      _this = this;
+      setTimeout(function() {
+        // makes sure that position: fixed is assigned to old activeText
+        activeText = text;
+        activeGrid = _this;
+      }, 300);
     }
     else {
       text.classList.remove('community-text--active');
       this.classList.remove('community-grid--active');
-      activeText = '';
-      activeGrid = '';
-      console.log('close');
+      // wait until transition is complete to change positioning
+      setTimeout(function() {
+        text.style.position = 'fixed';
+      }, 300);
+      activeText = null;
+      activeGrid = null;
     }
   }
 
